5da35410f34067182e4074310563c63d4db6c51f
Vlad <143434978+Dim-Vlad@users.noreply.github.com>
2024-07-03 18:28:33 +0200

    Rectification mainJS

--- js/main.js
+++ js/main.js
@@ -22,14 +22,3 @@ function loadHTML(file, elementId) {
       console.error('There has been a problem with your fetch operation:', error);
     });
 }
-
-// Fonction pour basculer le menu burger
-function toggleMenu() {
-  const navbarLinks = document.getElementById('navbarLinks');
-  navbarLinks.classList.toggle('show'); // Ajoute ou supprime la classe 'show' pour afficher/cacher les liens
-}
-
-// Ajoute un écouteur d'événements sur le bouton burger
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('.navbar-toggler').addEventListener('click', toggleMenu);
-});
