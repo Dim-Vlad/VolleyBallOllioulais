7c02bca31778ecb2fb8e23cbd9d3e8ca5bebf620
Vlad <143434978+Dim-Vlad@users.noreply.github.com>
2024-08-02 19:31:33 +0200

    Nous contacter + Envoyer photos

---
+++ php/send_mail.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Capture et nettoie les données du formulaire
+    $firstname = htmlspecialchars(trim($_POST['firstname']));
+    $lastname = htmlspecialchars(trim($_POST['lastname']));
+    $email = htmlspecialchars(trim($_POST['mail']));
+    $subject = htmlspecialchars(trim($_POST['subject']));
+
+    // Adresse email de destination
+    $to = 'votre_adresse_email@example.com';
+
+    // Sujet de l'email
+    $email_subject = "Nouveau message de $firstname $lastname";
+
+    // Corps du message
+    $email_body = "Nom: $firstname\nPrénom: $lastname\nEmail: $email\n\nMessage:\n$subject";
+
+    // En-têtes de l'email
+    $headers = "From: $email";
+
+    // Envoi de l'email
+    if (mail($to, $email_subject, $email_body, $headers)) {
+        echo "Message envoyé avec succès.";
+    } else {
+        echo "Une erreur est survenue, veuillez réessayer.";
+    }
+}
+?>
