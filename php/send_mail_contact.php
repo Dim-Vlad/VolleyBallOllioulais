8419dbaf10210fd06cde634759d130044e2af694
Vlad <143434978+Dim-Vlad@users.noreply.github.com>
2024-08-14 22:24:28 +0200

    Modif nousContacter "Ajout liste déroule OBJET"

--- php/send_mail_contact.php
+++ php/send_mail_contact.php
@@ -1,15 +1,27 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $to = 'dimitrigarrigues@gmail.com'; // Remplacez par votre email
-    $subject = 'Nouveau message de contact';
-    $message = 'Nom: ' . $_POST['lastname'] . "\n" .
-                'Prénom: ' . $_POST['firstname'] . "\n" .
-                'E-mail: ' . $_POST['mail'] . "\n\n" .
-                'Objet: ' . $_POST['objet'] . "\n" .
-                'Message: ' . $_POST['subject'];
-    $headers = 'From: webmaster@votresite.com';
+    $to = 'dimitrigarrigues@gmail.com';
 
-    if (mail($to, $subject, $message, $headers)) {
+    // Récupérer l'objet sélectionné dans la liste déroulante
+    $selected_subject = $_POST['subject-dropdown'];
+
+    // Créer un sujet d'email plus descriptif
+    $email_subject = 'Nouveau message de contact: ' . $selected_subject;
+
+    // Composer le corps du message
+    $message = 'Nom: ' . $_POST['lastname'] . "\n" .
+            'Prénom: ' . $_POST['firstname'] . "\n" .
+            'E-mail: ' . $_POST['mail'] . "\n\n" .
+            'Objet: ' . $selected_subject . "\n" .
+            'Message: ' . $_POST['subject'];
+
+    // Définir les en-têtes de l'email
+    $headers = 'From: webmaster@votresite.com' . "\r\n" .
+            'Reply-To: ' . $_POST['mail'] . "\r\n" .
+            'X-Mailer: PHP/' . phpversion();
+
+    // Envoyer l'email
+    if (mail($to, $email_subject, $message, $headers)) {
         echo 'Message envoyé avec succès';
     } else {
         http_response_code(500);
